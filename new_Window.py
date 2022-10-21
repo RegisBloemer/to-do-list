053a0f12616aa55cd7ace1943a414c833dd78f81
Régis Nyland Bloemer <regisnb1@gmail.com>
2022-10-21 19:12:34 -0300

    funcionalidades da nova tela adicionadas

--- new_Window.py
+++ new_Window.py
@@ -1,8 +1,26 @@
 import tkinter
 def open_new_window():
     new_window = tkinter.Tk()
-    new_window.geometry("500x500")
     new_window.resizable(False, False)
-    new_window.title("To-Do-List")
+    new_window.title("Tasks")
 
-    new_window.mainloop()
+    def add_task():
+        task = entry_task.get()
+        if task != "":
+            listbox_tasks.insert(tkinter.END, task)
+            entry_task.delete(0, tkinter.END)
+        else:
+            tkinter.messagebox.showerror(
+                title="Aviso!", message="Voce deve digitar o nome da task!")
+
+#Create de GUI
+    listbox_tasks = tkinter.Listbox(new_window, height=3, width= 50)
+    listbox_tasks.pack()
+
+    entry_task =tkinter.Entry(new_window, width=50 )
+    entry_task.pack()
+
+    button_add_task = tkinter.Button(new_window, text="Add task", width=48, command=add_task)
+    button_add_task.pack()
+
+    new_window.mainloop()   
