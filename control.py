19f9dc51bb9af6c35245468b8337796a6ea2cd8d
Régis Nyland Bloemer <regisnb1@gmail.com>
2022-10-23 15:54:48 -0300

    funções de CRUD e interação com a GUI finalizadas

---
+++ control.py
@@ -0,0 +1,67 @@
+import data_base
+
+class List:
+
+    #Inicializa os atributos name(string) e tasks(dicionário)  
+    def __init__(self, name: str, tasks: dict):
+        self.name: str = name
+        self.tasks: dict = tasks
+
+    #Função que adiciona a descrição da task com o id da task
+    def add_task(self, description: str, id: int):
+        self.tasks[id] = description
+
+    #Função que remove a task pelo seu id
+    def remove_task(self, id: int):
+        del self.tasks[id]
+
+    #Funções que que retorna as tasks e nome da lista
+    def get_tasks(self):
+        return self.tasks
+
+    def get_name(self):
+        return self.name
+
+class Control:
+
+    #Inicializa o dicionário de listas 
+    def __init__(self):
+        self.lists = {}
+
+    #Lê tabelas do banco de dados e converte para Listas de Tasks em Python
+    def load_all(self):
+        list_table = data_base.get_list_data_base() # [name, list_id]
+
+        for line in list_table:
+            task_table = data_base.get_task_from_list_data_base(line["list_id"])
+            self.lists[line["list_id"]] = List(line["name"], task_table)
+
+    #Funções de adição de listas e tasks na lista 
+    def add_list(self, name: str):
+        data_base.add_list_data_base(name)
+        new_id = data_base.get_last_list_id()
+        self.lists[new_id] = List(name, {})
+
+    def add_task_to_list(self, description: str, list_id: int):
+        data_base.add_task_data_base(description, list_id)
+        new_task_id = data_base.get_last_task_id()
+        self.lists[list_id].add_task(description, new_task_id)
+
+    #Funções de remoção da lista e task 
+    def remove_list(self, list_id: int):
+        data_base.remove_list_data_base(list_id)
+        del self.lists[list_id]
+
+    def remove_task_from_list(self, task_id: int, list_id: int):
+        data_base.remove_task_data_base(task_id)
+        self.lists[list_id].remove_task(task_id)
+
+    #Funções para pegar as listas e task para a GUI
+    def get_lists(self):
+        list_table = {}
+        for id in self.lists.keys():
+            list_table[id] = self.lists[id].get_name()
+        return list_table
+
+    def get_tasks_from_list(self, list_id: int):
+        return self.lists[list_id].get_tasks()
--- data_base.py
+++ data_base.py
@@ -1,12 +1,93 @@
 import sqlite3
 
+#Conexão com o banco de dados
 data_base = sqlite3.connect('data_base')
-
 cursor = data_base.cursor()
 
-cursor.execute("CREATE TABLE List (name tinytext,list_id integer PRIMARY KEY);")
+# ps é a função para preparação de strings para a query impedindo SQL injection (Prepared Statement)
+# É chamada em toda query
+def ps(_str: str):
+    var_str = _str.replace("\\", "\\\\")
+    var2_str = var_str.replace("\'", "\'\'")
+    #var3_str = var2_str.replace("\"", "\\\"")
+    return var2_str()
 
-cursor.execute("CREATE TABLE Task (description text,list_id integer,task_id integer PRIMARY KEY,FOREIGN KEY(list_id) REFERENCES List (list_id));")
+#Função para reconstruir o banco de dados
+def recostruct_data_base():
+    cursor.execute("DROP TABLE Task;")
+    cursor.execute("DROP TABLE List;")
+    cursor.execute(
+        "CREATE TABLE List (name tinytext,list_id integer PRIMARY KEY AUTOINCREMENT);")
+    cursor.execute(
+        "CREATE TABLE Task (description text,list_id integer,task_id integer PRIMARY KEY AUTOINCREMENT,FOREIGN KEY(list_id) REFERENCES List (list_id));")
+    data_base.commit()
 
-data_base.commit()
 
+#Funções de adição
+# Função que adiciona a lista pelo nome na tabela lista ao banco de dados
+def add_list_data_base(_name):
+    data_tasks = cursor.execute(
+        f"INSERT INTO List (name) VALUES ('{ps(_name)}');")
+
+#Função que adiciona a task na tabela Task no banco de dados
+def add_task_data_base(description, list_id: int):
+    cursor.execute(
+        f"INSERT INTO Task (name, list_id) VALUES ('{ps(description)}', {list_id});")
+
+
+#Funções de remoção
+#Função que deleta a task pelo seu task_id da tabela task no banco de dados
+def remove_task_data_base(task_id: int):
+    cursor.execute(f"DELETE FROM Task WHERE task_id = {task_id};")
+
+#Função que remove todas as task da tabela Task para que se possa excluir a Lista
+def remove_all_task_from_list_data_base(list_id: int):
+    cursor.execute(f"DELETE FROM Task WHERE list_id = {list_id}")
+
+#Função que remove a lista pelo seu list_id na tabela List
+def remove_list_data_base(list_id: int):
+    remove_all_task_from_list_data_base(list_id)
+    cursor.execute(f"DELETE FROM List WHERE list_id = {list_id};")
+
+#Funções para pegar as listas diretamente do banco de dados
+def get_list_data_base():
+    data_lists = cursor.execute(f"SELECT name, list_id FROM List")
+    table = data_lists.fetchall() # table = [ (name, list_id), ... ]
+    dict_table = []
+    for line in table: # line = (name, list_id)
+        dict_table.append({"name": line[0], "list_id": line[1]}) # [{"name":name, "list_id":list_id}, ...]
+    return dict_table #table foi trasformado em dicionário para facilitar o entendimento
+
+#Função que pega task das suas respectivas listas 
+def get_task_from_list_data_base(list_id: int):
+    data_tasks = cursor.execute(
+        f"SELECT description, task_id FROM Task WHERE list_id = {list_id}")    # Traduzindo tabela para um dicionário
+    table = data_tasks.fetchall() # table = [ (description, task_id), ... ]
+    task_table = {}
+    for line in table: # line = (description, task_id)
+        task_table[line[1]] = line[0] # {task_id: description, ...}
+    return task_table
+
+#Função para buscar o maior id na tabela Task
+def get_last_task_id():
+    data_tasks = cursor.execute(f"SELECT MAX(task_id) FROM Task")
+    res = data_tasks.fetchall() # Sempre vai retornar isso uma lista de tuplas : [(id)]
+    if len(res) == 0:
+        return 0
+    return res[0][0] # Tupla 0 item 0
+ 
+#Função para buscar o maior id na tabela List
+def get_last_list_id():
+    data_tasks = cursor.execute(f"SELECT MAX(list_id) FROM List")
+    res = data_tasks.fetchall()  #Sempre vai retornar isso uma lista de tuplas : [(id)]
+    if len(res) == 0:
+        return 0
+    return res[0][0] # Tupla 0 item 0
+
+# # Test
+# recostruct_data_base()
+# add_list_data_base("a")
+# add_list_data_base("s")
+# remove_list_data_base(2)
+# add_list_data_base("d")
+# print(get_list_data_base())
